ab1a6912415b47c873a8e15f577a202efd367765
bimantaramitrapersada <bimantaramitrapersada@gmail.com>
2026-07-22 10:21:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: https://script.google.com/macros/s/AKfycbyrUarByjFOZ65Fz8QXw3MxT-wINPPDKSFW87O8SPYvKNFoGZ7vfnk2KfWg4yQvKA41/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyrUarByjFOZ65Fz8QXw3MxT-wINPPDKSFW87O8SPYvKNFoGZ7vfnk2KfWg4yQvKA41/exec',
   SECRET_TOKEN: 'bimantaramitrapersadakediri',
- KANTOR: { 
-  lat: -7.78741542408522,
-  lng: 112.0201064024613},
+  KANTOR: { 
+    lat: -7.78741542408522,
+    lng: 112.0201064024613
+  },
   RADIUS_MAX: 100
 };
